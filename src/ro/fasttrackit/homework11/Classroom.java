bd6fc0f46eb0dd022c8d99c5284ed636db547202
Iaro <iaroslav.slovak@gmail.com>
2021-04-25 01:54:57 +0300

    Initial commit

--- src/ro/fasttrackit/homework11/Classroom.java
+++ src/ro/fasttrackit/homework11/Classroom.java
@@ -1,22 +1,63 @@
 package ro.fasttrackit.homework11;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Classroom {
     private final List<StudentGrade> classroom;
 
     public Classroom(List<StudentGrade> classroom) {
-        this.classroom = classroom;
+        this.classroom = new ArrayList<> ();
+        this.classroom.addAll (classroom);
     }
 
-    public List<StudentGrade> getGradesForDiscipline(String discipline) {
-        List<StudentGrade> result = new ArrayList<> ();
+    public ArrayList<Integer> getGradesForDiscipline(String discipline) {
+        ArrayList<Integer> result = new ArrayList<> ();
         for (StudentGrade student : classroom) {
             if (student.getDiscipline ().equals (discipline)) {
+                result.add (student.getGrade ());
+            }
+        }
+        return result;
+    }
+
+    public List<StudentGrade> getGradesForStudent(String name) {
+        List<StudentGrade> result = new ArrayList<> ();
+        for (StudentGrade student : classroom) {
+            if (student.getName ().equals (name)) {
                 result.add (student);
             }
         }
         return result;
+    }
+
+    public int getMaxDisciplineGrade(String discipline) {
+        List<Integer> result = getGradesForDiscipline (discipline);
+
+        return Collections.max (result);
+    }
+
+    public int getMaxGrade() {
+        List<Integer> result = new ArrayList<> ();
+        for (StudentGrade student : classroom) {
+            result.add (student.getGrade ());
+        }
+        return Collections.max (result);
+    }
+
+    public int getAverageGrade(String discipline) {
+        List<Integer> result = getGradesForDiscipline (discipline);
+        int sum = 0;
+        for (StudentGrade student : classroom) {
+            if (student.getDiscipline ().equals (discipline)) {
+                sum += student.getGrade ();
+            }
+        }
+        return sum / result.size ();
+    }
+
+    public int getWorstGrade(String discipline) {
+        List<Integer> result = getGradesForDiscipline (discipline);
+
+        return Collections.min (result);
     }
 }
--- src/ro/fasttrackit/homework11/ClassroomMain.java
+++ src/ro/fasttrackit/homework11/ClassroomMain.java
@@ -1,7 +1,27 @@
 package ro.fasttrackit.homework11;
 
+import java.util.List;
+
 public class ClassroomMain {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
+        Classroom classroom = new Classroom (
+                List.of (
+                        new StudentGrade ("Ealhstan Saira", "Computer Science", 10),
+                        new StudentGrade ("Gasparo Sava", "Mathematics", 7),
+                        new StudentGrade ("Demetria Cosme", "Mathematics", 9),
+                        new StudentGrade ("Popa Cristina", "Mathematics", 8),
+                        new StudentGrade ("Majken Olgica", "Mathematics", 4),
+                        new StudentGrade ("Majken Olgica", "History", 9),
+                        new StudentGrade ("Miriana Rómulo", "Computer Science", 7),
+                        new StudentGrade ("Utu Arcadius", "Physics", 3),
+                        new StudentGrade ("Branwen Evelina", "Physics", 4)
+                ));
+        System.out.println ("Grades for selected discipline: " + classroom.getGradesForDiscipline ("Mathematics"));
+        System.out.println ("Grade for student: " + classroom.getGradesForStudent ("Majken Olgica"));
+        System.out.println ("Max grade for discipline: " + classroom.getMaxDisciplineGrade ("Mathematics"));
+        System.out.println ("Max grade: " + classroom.getMaxGrade ());
+        System.out.println ("Average grade for discipline: " + classroom.getAverageGrade ("Mathematics"));
+        System.out.println ("Worst grade for discipline: " + classroom.getWorstGrade ("Mathematics"));
 
     }
 }
