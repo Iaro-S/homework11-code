91f1f5ea80dc7365a40714e88443af3e51076539
Iaro <iaroslav.slovak@gmail.com>
2021-04-24 21:40:55 +0300

    Initial commit

--- src/ro/fasttrackit/homework11/ReadStudentGradesFromFile.java
+++ src/ro/fasttrackit/homework11/ReadStudentGradesFromFile.java
@@ -36,24 +36,5 @@ public class ReadStudentGradesFromFile {
             List<StudentGrade> students = readObjectsFromFile ();
             students.forEach (System.out::println);
         }
-
-        private static List<StudentGrade> readObjectsFromFile() throws Exception {
-            List<StudentGrade> students = new ArrayList<> ();
-            BufferedReader fileReader = new BufferedReader (new FileReader ("files/grades.txt"));
-            String line;
-            while ((line = fileReader.readLine ()) != null) {
-                students.add (readStudent (line));
-            }
-            return students;
-        }
-
-        private static StudentGrade readStudent(String studentInfo) {
-            String[] studentData = studentInfo.split ("\\|");
-            String name = studentData[0];
-            String discipline = studentData[1];
-            int grade = Integer.parseInt (studentData[2]);
-
-            return new StudentGrade (name, discipline, grade);
-        }
     }
 }
